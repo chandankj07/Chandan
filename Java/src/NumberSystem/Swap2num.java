4b291510a2c4fb5f742f068c227c8ce26c0e5778
Admin <Admin@Admin-PC>
2023-03-14 18:33:03 +0530

    java logics

---
+++ Java/src/NumberSystem/Swap2num.java
@@ -0,0 +1,17 @@
+package NumberSystem;
+
+public class Swap2num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=1;
+int r=2;
+System.out.println("Before" +" "+ i +" "+r);
+int temp=i;
+i=r;
+r=temp;
+System.out.println("After" +" "+ i +" "+r);
+
+	}
+
+}
